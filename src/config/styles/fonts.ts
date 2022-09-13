30dfdf354900563f74606f0621f74841bf50b274
AbrasLeo <leonardoabras@hotmail.com>
2022-09-13 12:34:28 -0300

    Feat: Login Screen

---
+++ src/config/styles/fonts.ts
@@ -0,0 +1,9 @@
+const fonts = {
+  Heebo_Black: 'Heebo-Black',
+  Heebo_Bold: 'Heebo-Bold',
+  Heebo_ExtraBold: 'Heebo-ExtraBold',
+  Heebo_ExtraLight: 'Heebo-ExtraLight',
+  Heebo_Light: 'Heebo-Light'
+};
+
+export default fonts;
--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -4,27 +4,24 @@ import { ImageBackground } from 'react-native';
 import styled from 'styled-components/native';
 
 import backgroundImage from '@/assets/images/background.png';
+import LogoImage from '@/assets/images/logo.svg';
 
-const LoginScreen = () => {
+import InputLogin from '@/screens/Login/Input';
+
+const Login = () => {
   return (
     <StyledBackground source={backgroundImage}>
       <StyledContentLogo>
-        <Logo />
+        <LogoImage />
       </StyledContentLogo>
       <StyledView>
-        <StyledInputTitle>{'E-mail'}</StyledInputTitle>
-        <StyledTextInput keyboardType='email-address' placeholder='Enter your email' />
-      </StyledView>
-      <StyledView>
-        <StyledRow>
-          <StyledColumn>
-            <StyledInputTitle>{'Password'}</StyledInputTitle>
-            <StyledTextInput secureTextEntry={true} placeholder='Enter your password' />
-          </StyledColumn>
-          <StyledLoginButton>
-            <StyledText>{'Entrar'}</StyledText>
-          </StyledLoginButton>
-        </StyledRow>
+        <InputLogin
+          title='Email'
+          marginBottom={16}
+          keyboardType='email-address'
+          placeholder='Digite o seu email'
+        />
+        <InputLogin title='Password' showButton secureTextEntry placeholder='Digite a sua senha' />
       </StyledView>
     </StyledBackground>
   );
@@ -38,55 +35,11 @@ const StyledBackground = styled(ImageBackground)`
 `;
 
 const StyledContentLogo = styled.View`
-  width: 100%;
   margin-bottom: 50px;
 `;
 
-const StyledTextInput = styled.TextInput`
-  height: 24px;
-  margin-top: 5px;
-  padding: 0px;
-  color: white;
-`;
 const StyledView = styled.View`
-  width: 100%;
-  margin-bottom: 15px;
-  border-radius: 4px;
-  padding: 8px 16px;
-  background-color: ${({ theme }) => theme.colors.inputBackground};
+  align-self: stretch;
 `;
 
-const StyledInputTitle = styled.Text`
-  color: ${({ theme }) => theme.colors.grayText};
-  opacity: 0.5;
-`;
-
-const StyledRow = styled.View`
-  width: 100%;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const StyledColumn = styled.View`
-  width: 50%;
-  flex-direction: column;
-`;
-const StyledLoginButton = styled.TouchableOpacity`
-  width: 85px;
-  height: 36px;
-  align-self: center;
-  align-items: center;
-  justify-content: center;
-  padding: 8px 20px;
-  border-radius: 44px;
-  background-color: ${({ theme }) => theme.colors.white};
-`;
-
-const StyledText = styled.Text`
-  align-self: center;
-  font-size: 16px;
-  font-family: 'Heebo-Bold';
-  color: ${({ theme }) => theme.colors.primary};
-`;
-
-export default LoginScreen;
+export default Login;
